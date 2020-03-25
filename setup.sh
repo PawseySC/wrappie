dec58bd5328c080b73d10bcefb213578c6c54f87
Marco De La Pierre <marco.delapierre@gmail.com>
2020-03-25 07:09:10 +0000

    polishing setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,12 @@
 
 
 # relevant variables - can be customised
-rootdir="/data/work/wrappers-sgl/apps"
-moduledir="/data/work/wrappers-sgl/modules"
-# production directory where all data/analyses are
-# can also be a comma separated list 
-workdir="/data/work,/data/db"
+# root directory for applications (images+wrappers)
+rootdir=${rootdir:-"/data/work/wrappers-sgl/apps"}
+# root directory for modules
+moduledir=${moduledir:-"/data/work/wrappers-sgl/modules"}
+# production directory for data/analyses. only used for final output. can be a comma separated list 
+workdir=${workdir:-"/data/work,/data/db"}
 
 
 # prompt for list file
@@ -19,13 +20,17 @@ elif [ ! -s $1 ] ; then
 fi
 listfile=$1
 
-# create directories for images and wrapper scripts
-echo Creating image directory $rootdir ..
+# create directories for applications and modules
+echo Creating root directory for applications $rootdir ..
 mkdir -p $rootdir
+echo Creating root directory for modules $moduledir ..
+mkdir -p $moduledir
 
 # initialise list of tool directories
 appdir_list=""
 
+
+# main loop
 # read the list of requirements and do stuff
 while read line ; do
 
@@ -38,59 +43,58 @@ while read line ; do
  # is this line an image (0) or a command (1)?
  type=$(echo $line | grep '^-' -c)
 
+
  # it's an image
  if [ $type -eq 0 ] ; then
   # save name&infos
   image=$line
-  echo Saving infos for image $image ..
   siftmp=${image##*/}
   tool=${siftmp%:*}
   tag=${siftmp#*:}
   appdir="$rootdir/$tool/$tag"
   appdir_list+="${appdir}:"
   sif=${siftmp/:/_}.sif
-  # create directory
-  echo Creating directory for tool $tool tag $tag ..
-  mkdir -p $appdir
   # pull image
   echo Pulling container image $image ..
+  mkdir -p $appdir
   singularity pull --dir $appdir $sif $image
   # create module
-  echo Creating modulefile for tool $tool tag $tag ..
+  echo Creating module for tool $tool tag $tag ..
   mkdir -p $moduledir/$tool
-  cat << EOMM >$moduledir/$tool/$tag
+  cat << EOM >$moduledir/$tool/$tag
 #%Module1.0######################################################################
 ##
 ## $tool modulefile
 ##
 proc ModulesHelp { } {
-    puts stderr "\tThe $tool Module - tag $tag\n"
-    puts stderr "\tThis module allows you to use the container image $image."
+    puts stderr "\tModule for tool $tool , tag $tag\n"
+    puts stderr "\tThis module uses the container image $image."
 }
 
-module-whatis   "edits the PATH to use the tool $tool - tag $tag"
+module-whatis   "edits the PATH to use the tool $tool , tag $tag"
 
 prepend-path     PATH            $appdir
 
-EOMM
+EOM
 
 
  # it's a command, so create the wrapper script
  elif [ $type -eq 1 ] ; then
   cmd=${line##* }
   echo - Creating wrapper for command $cmd
-  cat << EOF >$appdir/$cmd
+  cat << EOW >$appdir/$cmd
 #!/bin/bash
 
 singularity exec $appdir/$sif $cmd "\$@"
-EOF
+EOW
   chmod +x $appdir/$cmd
  fi 
 
 done < $listfile
 
+
 # display information on how to configure shell environment
-cat << EOM
+cat << EOD
 
 All done!
 
@@ -101,4 +105,4 @@ module use $moduledir   #hpc-containers-wrappers
 export SINGULARITY_BINDPATH=\$SINGULARITY_BINDPATH,$workdir   #hpc-containers-wrappers
 ##############################
 
-EOM
+EOD
